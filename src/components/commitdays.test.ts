beda7bf69a53735a656fca27878e9c16398c4685
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2024-11-25 06:41:07 -0800

    combine commitdays

---
+++ src/components/commitdays.test.ts
@@ -0,0 +1,43 @@
+// myTest.test.ts
+import assert from 'node:assert';
+import path from 'node:path';
+import { readFileSync, writeFileSync, existsSync } from 'node:fs';
+import { test } from 'node:test';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import { daysToCells } from "./commitdays.ts";
+
+// Main test with subtests
+test('daysToCells()', { skip: false }, async (t) => {
+    // Define test cases inside the main test
+    const testCases = [
+        { name: 'simple', days: [] },
+        { name: 'a date', days: [new Date(2024, 10, 23, 21)] },
+    ];
+    const updateTests = process.env.OBC_UPDATE_TESTS === 'true';
+
+    // Run each test case as a subtest
+    for (const { name, days } of testCases) {
+
+        const goldenFile = path.resolve(__filename + "_testdata", `${name}.golden.json`);
+
+        await t.test(`${name}`, () => {
+            const actual = daysToCells(days);
+            if (updateTests) {
+                writeFileSync(goldenFile, JSON.stringify(actual, null, 2));
+                console.log(`Updated ${goldenFile}`);
+            }
+
+            if (!existsSync(goldenFile)) {
+                throw new Error(`Golden file not found: ${goldenFile}`);
+            }
+
+            const expected = JSON.parse(readFileSync(goldenFile, 'utf-8'));
+            assert.deepStrictEqual(actual, expected);
+        });
+    }
+});
--- src/components/commitdays.ts
+++ src/components/commitdays.ts
@@ -1,3 +1,5 @@
+import * as Plot from "@observablehq/plot";
+
 type Cell = {
     x: string,
     y: string,
@@ -41,3 +43,27 @@ export function daysToCells(days: Date[]): Cell[] {
     return data;
 }
 
+export function commitDays(data: Cell[]) {
+  return Plot.plot({
+    width: 1000,
+    height: 400,
+    marginLeft: 90,
+    marginRight: 10,
+    color: { type: "linear", scheme: "Greens" },
+    x: {
+      type: "band"
+    },
+    y: {
+      domain: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    },
+    marks: [
+      Plot.cell(data, {
+        x: "x",
+        y: "y",
+        sort: { x: "x", reverse: false, reduce: ([x]) => parseInt(x) },
+        fill: "fill"
+      }),
+      Plot.text(data, { x: 'x', y: 'y', text: 'fill', fill: 'black' }),
+    ]
+  })
+}
