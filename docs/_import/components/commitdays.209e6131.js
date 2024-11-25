aec8e0f2638d83541f98e6eacf6c7ee7f0934c9f
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2024-11-25 06:47:22 -0800

    local build

--- docs/_import/components/commitdays.209e6131.js
+++ docs/_import/components/commitdays.209e6131.js
@@ -1,3 +1,4 @@
+import * as Plot from "../../_node/@observablehq/plot@0.6.16/index.bac1276b.js";
 export function daysToCells(days) {
   const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let counter = {};
@@ -24,4 +25,28 @@ export function daysToCells(days) {
     }
   }
   return data;
+}
+export function commitDays(data) {
+  return Plot.plot({
+    width: 1e3,
+    height: 400,
+    marginLeft: 90,
+    marginRight: 10,
+    color: { type: "linear", scheme: "Greens" },
+    x: {
+      type: "band"
+    },
+    y: {
+      domain: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    },
+    marks: [
+      Plot.cell(data, {
+        x: "x",
+        y: "y",
+        sort: { x: "x", reverse: false, reduce: ([x]) => parseInt(x) },
+        fill: "fill"
+      }),
+      Plot.text(data, { x: "x", y: "y", text: "fill", fill: "black" })
+    ]
+  });
 }
--- docs/_import/components/timeline.3a99cfed.js
+++ docs/_import/components/timeline.3a99cfed.js
@@ -1,4 +1,4 @@
-import * as Plot from "../../_npm/@observablehq/plot@0.6.15/118fc3ce.js";
+import * as Plot from "../../_npm/@observablehq/plot@0.6.16/e828d8c8.js";
 
 export function timeline(events, {width, height} = {}) {
   return Plot.plot({
